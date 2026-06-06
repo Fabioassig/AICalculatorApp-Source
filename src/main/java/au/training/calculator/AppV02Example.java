0f1956ec317828539f616cf32dcbfdab5cdae451
Fabio <hightechrwp@gmail.com>
2026-06-06 12:13:12 +0500

    Add v0.2 learner guide example source file

---
+++ src/main/java/au/training/calculator/AppV02Example.java
@@ -0,0 +1,13 @@
+package au.training.calculator;
+
+public class AppV02Example {
+    public static void main(String[] args) {
+        System.out.println("AI Calculator v0.2");
+        System.out.println("2 + 3 = 5");
+        System.out.println("10 - 4 = 6");
+        System.out.println("5 x 6 = 30");
+        System.out.println("20 / 4 = 5");
+        System.out.println("Release update: square calculation added");
+        System.out.println("4 squared = 16");
+    }
+}
